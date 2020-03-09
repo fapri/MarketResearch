eb36fdf45302c88d1ca3814005047064f7eafd0f
enscully <47668435+enscully@users.noreply.github.com>
2020-03-09 11:21:17 -0500

    Updated to include soybeans and oct/jan data pulls

--- Basis/IDW/getEikonData.R
+++ Basis/IDW/getEikonData.R
@@ -13,7 +13,7 @@ allBasis = data.frame(matrix(ncol = 5, nrow = 0))
 temp = data.frame()
 for (i in 1:nrow(locations)) {
   temp = get_data(instruments = list(locations$Instrument[i]),
-                  fields = list("CF_NAME", "CF_BID", "CF_DATE", "ROW80_1", "ROW80_2", "GN_TXT16_2", "GN_TXT16_4", "ROW80_3"))
+                  fields = list("CF_NAME", "CF_BID", "CF_DATE", "GEN_TEXT16", "ROW80_1", "ROW80_2", "GN_TXT16_2", "GN_TXT16_4", "ROW80_3"))
   allBasis = rbind(allBasis, cbind(temp, "Location" = rep(levels(droplevels(locations$CF_NAME[i])), length.out = nrow(temp))))
   temp = data.frame()
   print(i)
@@ -21,52 +21,97 @@ for (i in 1:nrow(locations)) {
 
 spotOnly = allBasis[which(grepl("SPOT", allBasis$CF_NAME) == TRUE), ]
 
-# saveRDS(spotOnly, file = "/Users/ensxvd/Desktop/spotOnly.rds")
-# saveRDS(allBasis, file = "/Users/ensxvd/Desktop/allBasis.rds")
+write.csv(spotOnly, 
+          file = paste("/Users/ensxvd/Desktop/refinitivData/", "cornSpotOnly", format(Sys.Date(), "%Y%m%d"), ".csv", sep = ""), 
+          row.names = FALSE)
+write.csv(allBasis, 
+          file = paste("/Users/ensxvd/Desktop/refinitivData/", "cornAllBasis", format(Sys.Date(), "%Y%m%d"), ".csv", sep = ""), 
+          row.names = FALSE)
+
+
+
+#####################
+# SOYBEANS
+#####################
+
+
+
+
+# Get basis data from Refinitiv
+
+library(usethis)
+library(devtools)
+library(eikonapir)
+
+set_app_id("")
+
+locations = get_data(instruments = list("0#GGC-SOYB-MO"),
+                     fields = list("CF_NAME"))
+
+allBasis = data.frame(matrix(ncol = 5, nrow = 0))
+temp = data.frame()
+for (i in 1:nrow(locations)) {
+  temp = get_data(instruments = list(locations$Instrument[i]),
+                  fields = list("CF_NAME", "CF_BID", "CF_DATE", "GEN_TEXT16", "ROW80_1", "ROW80_2", "GN_TXT16_2", "GN_TXT16_4", "ROW80_3"))
+  allBasis = rbind(allBasis, cbind(temp, "Location" = rep(levels(droplevels(locations$CF_NAME[i])), length.out = nrow(temp))))
+  temp = data.frame()
+  print(i)
+}
+
+spotOnly = allBasis[which(grepl("SPOT", allBasis$CF_NAME) == TRUE), ]
+
+write.csv(spotOnly, 
+          file = paste("/Users/ensxvd/Desktop/refinitivData/", "soybeanSpotOnly", format(Sys.Date(), "%Y%m%d"), ".csv", sep = ""), 
+          row.names = FALSE)
+write.csv(allBasis, 
+          file = paste("/Users/ensxvd/Desktop/refinitivData/", "soybeanAllBasis", format(Sys.Date(), "%Y%m%d"), ".csv", sep = ""), 
+          row.names = FALSE)
 
 
 
 
 
-########################################################################
-# Compare Corn and soybean addresses
-########################################################################
 
-# cornSpotOnly = read_csv("Basis/refinitivData/cornSpotOnly02282020.csv")
-# # Remove extra columns
-# cornSpotOnly = subset(cornSpotOnly, select = -c(GEN_TEXT16, Location))
-# # Change column names
-# colnames(cornSpotOnly) = c("instrument", "contractName", "basis", "date", "terminalName", "address", "county", "cropType", "phoneNumber")
-# # Get zip codes
-# cornSpotOnly$zipCode = str_extract(cornSpotOnly$phoneNumber, "\\d{5}")
-# # Paste zip codes to address to send to geocoder
-# cornSpotOnly$geoFormatAddress = paste(cornSpotOnly$address, cornSpotOnly$zipCode, sep = ", ")
-# 
-# soybeanSpotOnly = read_csv("Basis/refinitivData/soybeanSpotOnly02282020.csv")
-# # Remove extra columns
-# soybeanSpotOnly = subset(soybeanSpotOnly, select = -c(GEN_TEXT16, Location))
-# # Change column names
-# colnames(soybeanSpotOnly) = c("instrument", "contractName", "basis", "date", "terminalName", "address", "county", "cropType", "phoneNumber")
-# # Get zip codes
-# soybeanSpotOnly$zipCode = str_extract(soybeanSpotOnly$phoneNumber, "\\d{5}")
-# # Paste zip codes to address to send to geocoder
-# soybeanSpotOnly$geoFormatAddress = paste(soybeanSpotOnly$address, soybeanSpotOnly$zipCode, sep = ", ")
-# 
-# 
-# # Get important information
-# cornSubset = cornSpotOnly[, c("instrument", "geoFormatAddress")]
-# 
-# # # Confirm that soybean terminal names are the same as corn
-# # soybeanSubset = soybeanSpotOnly
-# # soybeanSubset = gsub('SOYB', 'CORN', soybeanSubset$instrument)
-# # checkAddress = merge(x = soybeanSubset, y = cornSubset, by = c("geoFormatAddress", "instrument"), all.x = TRUE)
-# 
-# # Merge the sets by address (before being cleaned)
-# # All addresses matched up to corn, meaning that the
-# # errors we saw in corn are consistent with soybeans
-# checkAddress = merge(x = soybeanSpotOnly, y = cornSubset, by = c("geoFormatAddress"), all.y = TRUE)
-# 
-# # Get the instruments which do not carry from corn and
-# # ensure they are not in cleaning functions
-# notInSoybeans = which(is.na(checkAddress$instrument.x))
-# checkAddress$instrument.y[notInSoybeans]
+
+
+
+
+spotOnly$Instrument = as.character(spotOnly$Instrument)
+
+# October
+oct = data.frame()
+
+for (i in 1:nrow(spotOnly)) {
+  oct = rbind(oct, cbind("Instrument" = spotOnly$Instrument[i], "octAvg" = mean(as.numeric(get_timeseries(spotOnly$Instrument[i],
+                                                                                                          list("*"),
+                                                                                                          start_date = "2019-10-01T15:04:05",
+                                                                                                          end_date = "2019-10-31T15:04:05")$VALUE), na.rm = TRUE)))
+  print(i)
+}
+
+oct$Instrument = as.character(oct$Instrument)
+oct$octAvg = as.numeric(levels(oct$octAvg))[oct$octAvg]
+
+# January
+jan = data.frame()
+
+for (i in 1:nrow(spotOnly)) {
+  jan = rbind(jan, cbind("Instrument" = spotOnly$Instrument[i], "janAvg" = mean(as.numeric(get_timeseries(spotOnly$Instrument[i],
+                                                                                                          list("*"),
+                                                                                                          start_date = "2020-01-01T15:04:05",
+                                                                                                          end_date = "2020-01-31T15:04:05")$VALUE), na.rm = TRUE)))
+  print(i)
+}
+
+jan$Instrument = as.character(jan$Instrument)
+jan$janAvg = as.numeric(levels(jan$janAvg))[jan$janAvg]
+
+
+# Merge Oct and Jan to existing data
+spotOnly = merge(x = spotOnly, y = oct, by = "Instrument", all.x = TRUE)
+spotOnly = merge(x = spotOnly, y = jan, by = "Instrument", all.x = TRUE)
+
+# write.csv(spotOnly, file = "/Users/ensxvd/Desktop/spotJanOct.csv", row.names = FALSE)
+
+
+
