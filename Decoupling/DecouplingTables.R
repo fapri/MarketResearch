3c05b32086895e861f6343aaa447552dae8ba22f
enscully <ensxvd@mail.missouri.edu>
2020-04-10 11:52:17 -0500

    Removed the need for the list template to be imported. Cleaned everything up and added comments

--- Decoupling/DecouplingTables.R
+++ Decoupling/DecouplingTables.R
@@ -1,4 +1,6 @@
-
+# Emily Scully
+# Created March 31, 2020
+# Project: Decoupling Data Organization and Tables
 
 # Load libraries
 library(readxl)
@@ -14,51 +16,44 @@ library(officer)
 # Load data
 DcData = read_excel("Decoupling/DecouplingLitData-X.xlsx", 
                     col_names = FALSE)
-templateDf = read.csv("Decoupling/template.csv", header = FALSE)
-row.names(templateDf) = templateDf$V1
-templateDf$V1 = NA
-
-templateList <- list()
-for (name in row.names(templateDf)) {
-  templateList[[name]] = list("data" = NA, "count" = NA, "average" = NA)
-}
-
-# Initialize lists
-all = est = notEst = usNat = other = templateList
 
 # Renumber columns
-
 colnames(DcData) = paste0("c", seq(from = 1, to = ncol(DcData), by = 1))
 
+# Clean data
 DcData$c1 = gsub("NEW SECTION", NA, DcData$c1)
 DcData$c1 = gsub(" - NOT USED FOR NOW", "", DcData$c1)
 DcData = DcData[rowSums(is.na(DcData)) != ncol(DcData),]
 DcData = DcData[-grep("Notes", DcData$c2), ]
-
 DcData$c1[grep("Nature of supply variable", DcData$c2)] = "Nature of supply variable"
 DcData$c2[grep("Nature of supply variable", DcData$c2)] = NA
 DcData$c1[grep("Cross effects among crops other than", DcData$c2)] = "Other cross effects"
 DcData$c2[grep("Cross effects among crops other than", DcData$c2)] = NA
 
+# fills in main categories so they can be matched to subcategories and to remove NAs
 for (row in 1:(nrow(DcData) - 1)) {
   if (!is.na(DcData$c1[row]) & is.na(DcData$c1[row + 1])) {
     DcData$c1[row + 1] = DcData$c1[row]
   }
 }
 
+# Remove rows with only NAs
 DcData = DcData[-(which(is.na(DcData$c2[9:nrow(DcData)])) + 8), ]
 
-# x = DcData[,c(1,2)]
-
+# removes () and any info in them
 DcData$c2 = gsub("\\s*\\([^\\)]+\\)","", as.character(DcData$c2))
+
+# removes punctuation
 DcData$c1 = gsub("[[:punct:]]+","", DcData$c1)
 DcData$c2 = gsub("[[:punct:]]+","", DcData$c2)
 
+# formatting/standardizing
 DcData$c1 = str_to_title(DcData$c1)
 DcData$c2 = str_to_title(DcData$c2)
 DcData$c1 = gsub(" ", "", DcData$c1)
 DcData$c2 = gsub(" ", "", DcData$c2)
 
+# Shortening names that are excessively long
 DcData$c1[grep("ProgramToWhichResultsRelate", DcData$c1)] = "Program"
 DcData$c1[grep("NatureOfSupplyVariable", DcData$c1)] = "NatureOfSupply"
 DcData$c2[grep("Peerreviewed", DcData$c2)] = "PeerReviewed"
@@ -74,12 +69,16 @@ DcData$c2[grep("ListCropOrCrops", DcData$c2)] = "CropOrCrops"
 DcData$c2[grep("PercentChangeInOutputPerunitPaymentDollarOrEuro", DcData$c2)] = "PctChangeOutputPerunitPmt"
 DcData$c2[grep("RatioOfPaymentImpactToMarketImpact", DcData$c2)] = "RatioOfPmtImpToMarketImp"
 
+# merge the two categories of labels together, separated by _
 DcData$c3 = paste(DcData$c1, DcData$c2, sep = "_")
 
 # Convert the data to a list for key/value access
 tDf = t(DcData)
 colnames(tDf) = row.names(tDf) = NULL
 
+# Converts each data frame row into a list
+# This is helpful for being able to call the variables by 
+# their key, rather than col/row notation
 DcList = lapply(seq_len(ncol(tDf[4:nrow(tDf), ])), function(i) tDf[4:nrow(tDf), ][,i])
 names(DcList) = tDf[3, ]
 
@@ -92,10 +91,41 @@ DcList = lapply(DcList, function(col) {
   }
 })
 
+# Create temp vector of the avenues
+tempAvenues = c("PriceEffect_",
+                "RiskAversion_",
+                "Wealth_",
+                "UpdatingAndExpectations_",
+                "ExemptionsOrExclusions_",
+                "CreditLiquidity_",
+                "Labor_",
+                "EntryOrExit_",
+                "Other_",
+                "All_")
+
+# Create vector of values needed for table
+factors = c("Relavancy",
+            "PCOPUP",
+            "PI2MI")
+
+# match each avenue to all factors
+avenues = vector()
+for (i in seq(length(tempAvenues))) {
+  avenues = c(avenues, paste0(tempAvenues[i], factors))
+}
+
+# Convert to a list of lists for key/value access
+templateList = list()
+for (i in avenues) {
+  templateList[[i]] = list("data" = NA, "count" = NA, "average" = NA)
+}
+
+# Initialize lists
+all = est = notEst = usNat = other = templateList
 
 ###############################################################
 
-
+# Set program choices for user
 programChoices = c("All during period",
                    "Crop insurance",
                    "ARC",
@@ -112,19 +142,23 @@ programChoices = c("All during period",
                    "Other"
 )
 
+# Set supply choices for user
 supplyChoices = c("Area of a Crop or Crops",
                   "Yield",
                   "Production of a Crop or Crops",
                   "All Supply Side Variables")
 
+# # Dialogue boxes for user input
 # selectedProgramText = dlgList(programChoices, preselect = NULL, multiple = FALSE,
 #                           title = "Program Type")$res
 selectedProgramText = "Fixed direct payment (contract payment)"
 
+# # Dialogue boxes for user input
 # selectedSupplyText = dlgList(supplyChoices, preselect = NULL, multiple = FALSE,
 #                          title = "Supply Type")$res
 selectedSupplyText = "Area of a Crop or Crops"
 
+# Convert user selection to list key name for access by name
 selectedProgram = switch(selectedProgramText, 
                          "All during period" = "Program_AllDuringPeriod",
                          "Crop insurance" = "Program_CropInsurance",
@@ -142,6 +176,7 @@ selectedProgram = switch(selectedProgramText,
                          "Other" = "Program_Other"
 )
 
+# Convert user selection to list key name for access by name
 selectedSupply = switch(selectedSupplyText, 
                         "Area of a Crop or Crops" = c("NatureOfSupply_AreaOfACrop", 
                                                       "NatureOfSupply_AreaOfAllOrManyCrops"),
@@ -164,20 +199,9 @@ if (length(selectedSupply) > 1) {
   supplyMaster = DcList[[selectedSupply]]
 }
 
-# Match list names
-names(DcList)[grep("DecouplingEffect_PriceEffect", names(DcList))] = "DecouplingEffect_priceEffect"
-names(DcList)[grep("DecouplingEffect_RiskAversion", names(DcList))] = "DecouplingEffect_riskAversion"
-names(DcList)[grep("DecouplingEffect_Wealth", names(DcList))] = "DecouplingEffect_wealth"
-names(DcList)[grep("DecouplingEffect_UpdatingAndExpectations", names(DcList))] = "DecouplingEffect_updating"
-names(DcList)[grep("DecouplingEffect_ExemptionsOrExclusions", names(DcList))] = "DecouplingEffect_exemptions"
-names(DcList)[grep("DecouplingEffect_CreditLiquidity", names(DcList))] = "DecouplingEffect_liquidity"
-names(DcList)[grep("DecouplingEffect_Labor", names(DcList))] = "DecouplingEffect_labor"
-names(DcList)[grep("DecouplingEffect_EntryOrExit", names(DcList))] = "DecouplingEffect_entryExit"
-names(DcList)[grep("DecouplingEffect_Other", names(DcList))] = "DecouplingEffect_other"
-names(DcList)[grep("DecouplingEffect_All", names(DcList))] = "DecouplingEffect_all"
-
 # Create sublists of the calculations
 calcLists = function(subList, programId) {
+  # Extra factors are other conditions to consider, such as studies covering all of the US
   if (programId == "all") {
     extraFactors = 1
   } else if (programId == "est") {
@@ -196,6 +220,7 @@ calcLists = function(subList, programId) {
     extraFactors = otherRelavancy
   }
   
+  # Pull relavancy, calculate averages, and get a count of observations
   for (i in grep("Relavancy", names((subList)))) {
     avenue = which((sub("\\_.*", "", names(subList))) == sub("\\_.*", "", names((subList[i]))))
     
@@ -203,8 +228,7 @@ calcLists = function(subList, programId) {
     PCOPUP = grep("PCOPUP", names(subList[avenue]))
     PI2MI = grep("PI2MI", names(subList[avenue]))
     
-    subList[[avenue[Relavancy]]][["data"]] = programMaster * supplyMaster * 
-      extraFactors * 
+    subList[[avenue[Relavancy]]][["data"]] = programMaster * supplyMaster * extraFactors * 
       DcList[[which((sub(".*_", "", names(DcList))) == sub("\\_.*", "", names((subList[i]))))]]
     subList[[avenue[PCOPUP]]][["data"]] = subList[[avenue[Relavancy]]]$data * DcList[["ImpactOfPayments_PctChangeOutputPerunitPmt"]]
     subList[[avenue[PI2MI]]][["data"]] = subList[[avenue[Relavancy]]]$data * DcList[["Ratio_RatioOfPmtImpToMarketImp"]]
@@ -242,18 +266,23 @@ other = calcStats(other)
 
 ###############################################################
 
-
-tableTemplate = read_excel("Decoupling/testTable.xlsx", col_names = TRUE)
+# Template for loading data into the table
+tableTemplate = read_excel("Decoupling/tableTemplate.xlsx", col_names = TRUE)
 tableTemplate = as.data.frame(tableTemplate)
 
+# Dynamic program identifier
 tableTemplate[1, ] = selectedProgramText
 
+# Create the flex table
 myft = flextable(tableTemplate)
 
+# Merge duplicate columns, row-wise
 myft = merge_at(myft, i = 1)
 myft = merge_at(myft, i = 2, j = 1:8)
 myft = merge_at(myft, i = 14, j = 1:6)
 
+# Rename columns
+# These cannot be put into the template because the column names would not be unique
 myft = set_header_labels(myft, values = list(V1 = " ",
                                              V2 = " ",
                                              V3 = " ",
@@ -278,25 +307,23 @@ myft = set_header_labels(myft, values = list(V1 = " ",
                                              V22 = "All",
                                              V23 = "All"))
 
+# Merge duplicate columns in the header
 myft = merge_h(myft, part = "header")
 
+# Align some rows to the left
 myft = align(myft, i = c(1,2,14), align = "left")
 
-std_border = fp_border(color = "black", style = "solid", width = 2)
-myft = hline_bottom(myft, part = "header", border = std_border)
-myft = hline_top(myft, part = "header", border = std_border)
-
-myft = merge_h(myft, part = "header")
-
-# myft = border_inner(myft, border = std_border, part = "all")
-
+# Fix the column widths
 myft = width(myft, j = 1:3, width = 1.5)
 myft = width(myft, j = 4:23, width = 0.5)
 
+# Align title center
 myft = align(myft, align = "center", part = "header")
+
+# Align some data to the left
 myft = align(myft, j = c(5,7,9,11,13,15,17,19,21,23), align = "left", part = "body")
 
-
+# Text styling
 myft = style(myft, i = c(1, 2, 14), pr_t = fp_text(color = "black", bold = TRUE), part = "body")
 myft = style(myft, pr_t = fp_text(color = "black", bold = TRUE, font.size = 10), part = "header")
 
@@ -304,12 +331,4 @@ myft = style(myft, pr_t = fp_text(color = "black", bold = TRUE, font.size = 10),
 set_caption(myft, paste0("Avenue of Payment Impact on ", 
                          selectedSupplyText, 
                          ", Number of Observations. and Simple Average"))
-
-
-
-
-
-
-
-
 
